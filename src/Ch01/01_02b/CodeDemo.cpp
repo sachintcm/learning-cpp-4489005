65c172e37715a42098aaf676001f1071a8e003b8
sachintcm <sachin.tcm2692@gmail.com>
2026-02-24 08:15:15 +0000

    changes are made to chapter1 and chapter2_5th section

--- src/Ch01/01_02b/CodeDemo.cpp
+++ src/Ch01/01_02b/CodeDemo.cpp
@@ -1,3 +1,19 @@
 // Learning C++ 
 // Exercise 01_02
 // Hello World, by Eduardo Corpeño 
+
+#include <iostream>
+#include <string>
+
+int main()
+{
+    std::string str;
+    std::cout << "Enter you name \n" << std::flush;
+    std::cin >> str;
+    std::cout << "Nice to meet you " << str << "!" << std::endl;
+
+    std::cout << std::endl << std::endl;
+
+    return 0;
+
+}
--- src/Ch02/02_05b/CodeDemo.cpp
+++ src/Ch02/02_05b/CodeDemo.cpp
@@ -6,6 +6,21 @@
 #include <typeinfo>
 
 int main(){
+    auto a = 3;
+    auto b = 12345678910;
+    auto c = 3.14f;
+    auto d = 3.14;
+    auto e = true;
+    auto f = 'a';
+    auto g = "C++ Rocks!";
+
+    std::cout << "The type of a is " << typeid(a).name() << std::endl;
+    std::cout << "The type of b is " << typeid(b).name() << std::endl;
+    std::cout << "The type of c is " << typeid(c).name() << std::endl;
+    std::cout << "The type of d is " << typeid(d).name() << std::endl;
+    std::cout << "The type of e is " << typeid(e).name() << std::endl;
+    std::cout << "The type of f is " << typeid(f).name() << std::endl;
+    std::cout << "The type of g is " << typeid(g).name() << std::endl;
     
     std::cout << std::endl << std::endl;
     return (0);
